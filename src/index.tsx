f33481a21c8b3e83441754d45801e87e894bf48b
Gergely Pap <gergely.pap.sp@lhsystems.com>
2020-01-22 12:10:35 +0100

    Remove error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -4,29 +4,7 @@ import App from './App';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
-class ErrorBoundary extends React.Component {
-  public static getDerivedStateFromError(error: Error) {
-    return { error };
-  }
-  public state = {
-    error: null,
-  };
-
-  public render() {
-    if (this.state.error) {
-      console.error(this.state.error);
-      return 'Error occured, see console';
-    }
-    return this.props.children;
-  }
-}
-
-ReactDOM.render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
