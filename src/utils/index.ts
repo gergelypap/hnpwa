b42eb9e993b035d1980aab72a62479550d4c3c1d
Gergely Pap <pgege42@gmail.com>
2021-01-20 14:37:19 +0100

    Use Intl.RelativeTimeFormat to format post dates

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,28 +10,27 @@ export function pluralize(
   return String(count) + ' ' + (count === 1 ? singular : plural);
 }
 
-export const timeAgo = (timestamp: number) => {
-  // TODO: Use https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RelativeTimeFormat/format if supported.
-  // TODO: Refactor this function.
-  const msPerMinute = 60 * 1000;
+export function timeAgo(timestamp: number) {
+  const msPerMinute = 60000;
   const msPerHour = msPerMinute * 60;
   const msPerDay = msPerHour * 24;
   const msPerMonth = msPerDay * 30;
   const msPerYear = msPerDay * 365;
 
   const elapsed = Math.round(new Date().getTime()) - timestamp * 1000;
+  // TODO: Switch 'en' to application language.
+  const relativeTime = new Intl.RelativeTimeFormat('en', { style: 'long' });
 
   if (elapsed < msPerMinute) {
-    return Math.round(elapsed / 1000) + ' seconds ago';
+    return relativeTime.format(-Math.round(elapsed / 1000), 'second');
   } else if (elapsed < msPerHour) {
-    return Math.round(elapsed / msPerMinute) + ' minutes ago';
+    return relativeTime.format(-Math.round(elapsed / msPerMinute), 'minute');
   } else if (elapsed < msPerDay) {
-    return Math.round(elapsed / msPerHour) + ' hours ago';
+    return relativeTime.format(-Math.round(elapsed / msPerHour), 'hour');
   } else if (elapsed < msPerMonth) {
-    return Math.round(elapsed / msPerDay) + ' days ago';
+    return relativeTime.format(-Math.round(elapsed / msPerDay), 'day');
   } else if (elapsed < msPerYear) {
-    return Math.round(elapsed / msPerMonth) + ' months ago';
-  } else {
-    return Math.round(elapsed / msPerYear) + ' years ago';
+    return relativeTime.format(-Math.round(elapsed / msPerMonth), 'month');
   }
+  return relativeTime.format(-Math.round(elapsed / msPerYear), 'year');
 };
